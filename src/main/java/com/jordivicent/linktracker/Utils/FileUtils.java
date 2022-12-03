cbf35a2b0b87f1cb0452194a14bc98682e322985
vicentjordi <vicentmartinezjordi@gmial.com>
2022-12-03 09:27:39 +0100

    Dia2:Inici load.Pages()

--- src/main/java/com/jordivicent/linktracker/Utils/FileUtils.java
+++ src/main/java/com/jordivicent/linktracker/Utils/FileUtils.java
@@ -2,18 +2,25 @@ package com.jordivicent.linktracker.Utils;
 
 import com.jordivicent.linktracker.Model.WebPage;
 
+
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileUtils {
 
     public static List<WebPage> loadPages(Path file){
         System.out.println(file);
         try{
-
+             return Files.lines(Paths.get(String.valueOf(file)))
+                    .map(line -> new WebPage(line.split(";")[0],
+                            line.split(";")[1]))
+                    .collect(Collectors.toList());
         }catch (Exception e){
             MessageUtils.errorFormato(file);
-        }
-        return null;
+            return null;
+        }//end_try/catch
     }//end_loadPages
 }
